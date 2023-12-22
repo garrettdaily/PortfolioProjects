df1f5f51eb814c2228e1f1975aa8d15b3776b00b
Garrett Daily <153771233+garrettdaily@users.noreply.github.com>
2023-12-22 16:26:00 -0500

    SQLProject1.sql

--- SQLProject1.sql
+++ SQLProject1.sql
@@ -1,3 +1,6 @@
+--Covid 19 Data Exploration 
+--Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
 ----- 1)
 Select *
 From PortfolioProject..CovidDeaths$
